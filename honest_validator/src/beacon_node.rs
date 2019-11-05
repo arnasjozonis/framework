ebe4640c790a625239e129f2cd323ad88f6cf5f5
Arnas Jozonis <ajoz@danskebank.lt>
2019-11-05 16:42:58 +0200

    add getting proposer index from bn

--- honest_validator/src/beacon_node.rs
+++ honest_validator/src/beacon_node.rs
@@ -7,6 +7,7 @@ pub trait BeaconNode<C: Config> {
     fn compute_start_slot_at_epoch(&self, epoch: Epoch) -> Slot;
     fn get_committee_count_at_slot(&self, state: BeaconState<C>, slot: Slot) -> u64;
     fn get_beacon_committee(&self, state: BeaconState<C>, slot: Slot, index: CommitteeIndex) -> Vec<ValidatorIndex>;
+    fn get_beacon_proposer_index(&self, state: BeaconState) -> ValidatorIndex:
 }
 
 pub struct BasicBeaconNode<C: Config>{
