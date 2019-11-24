c7b717e415b4d0aee3cb1fdde45de9485bf58140
Arnas Jozonis <ajoz@danskebank.lt>
2019-11-24 16:17:10 +0200

    create rest client and deserialize one of bn endpoint responses

--- honest_validator/src/rest_client.rs
+++ honest_validator/src/rest_client.rs
@@ -1,8 +1,26 @@
 extern crate hyper;
+extern crate serde;
+extern crate serde_json;
 
 use std::io::{self, Write};
-use hyper::Client;
+use hyper::{Client, Error};
 use hyper::rt::{self, Future, Stream};
+use hyper::body::{Payload, Body };
+use std::str;
+use serde::{Deserialize, Serialize};
+use serde_json::{Result, from_str, Error as SerializeError};
+
+#[derive(Serialize, Deserialize)]
+struct Validator {
+    pubkey: String,
+    withdrawal_credentials: String,
+    effective_balance:u128,
+    slashed:bool,
+    activation_eligibility_epoch:u64,
+    activation_epoch:u64,
+    exit_epoch:u128,
+    withdrawable_epoch:u128
+}
 
 pub struct RestClient {
 }
@@ -14,22 +32,29 @@ impl RestClient {
             let uri = "http://localhost:5052/beacon/validators".parse().unwrap();
 
             client
-            .get(uri)
-            .and_then(|res| {
-                println!("Response: {}", res.status());
-                println!("Headers: {:#?}", res.headers());
-
-                res.into_body().for_each(|chunk| {
-                    io::stdout().write_all(&chunk)
-                        .map_err(|e| panic!("example expects stdout is open, error={}", e))
+                .get(uri)
+                .and_then(|res| res.into_body().concat2())
+                .and_then(|c| {
+                    Ok(
+                        str::from_utf8(&c)
+                        .map(|res| {
+                            let r: Vec<Validator> =  match serde_json::from_str(res){
+                                Ok(r) => r,
+                                _ => Vec::new()
+                            };
+                            println!("{}", r.first().unwrap().pubkey);
+                            res.to_owned()
+                        })
+                        .expect("The body could not be parsed as a UTF-8 string !")
+                    )
+                })
+                .map(|_| {
+                    println!("\n\nDone.");
+                })
+                .map_err(|err| {
+                    eprintln!("Error {}", err);
                 })
             })
-            .map(|_| {
-                println!("\n\nDone.");
-            })
-            .map_err(|err| {
-                eprintln!("Error {}", err);
-            })
-        }));
+        )
     }
 }
