5984f7d8144749fdd2f08eaed0756cbc7d8ce96f
Šmigelskytė Rasa <rasa.smigelskyte@mif.stud.vu.lt>
2019-12-17 15:35:34 +0200

    block signing

--- honest_validator/src/block_producer/mod.rs
+++ honest_validator/src/block_producer/mod.rs
@@ -1,48 +1,188 @@
-use crate::beacon_node::{BasicBeaconNode, BeaconNode, Error};
-use types::types::{SignedBeaconBlock, BeaconBlock, Eth1Data};
-use types::config::{Config as EthConfig, MinimalConfig};
-use types::primitives::{Epoch, ValidatorIndex};
-use bls::PublicKeyBytes;
-use bls::{SecretKey, Signature};
+use crate::rest_client::RestClient;
+use serde::{Deserialize, Serialize};
 use types::beacon_state::BeaconState;
-use std::{thread, time};
+use types::config::{MinimalConfig};
+use types::primitives::{CommitteeIndex, Domain, DomainType, Epoch, Slot, H256, ValidatorIndex};
+use types::types::{Attestation,BeaconBlock};
+use std::rc::Rc;
+use bls::PublicKeyBytes;
 
+const SLOTS_PER_HISTORICAL_ROOT: Slot = 8192;
+const SLOTS_PER_EPOCH: u64 = 8;
 
+#[derive(PartialEq, Debug)]
+pub enum Error {
+    SlotOutOfRange,
+    IndexOutOfRange,
+    ApiError,
+    AttestionPublishingError
+}
 
-pub fn produceBlock(beacon_node: &BasicBeaconNode, parent: BeaconBlock<MinimalConfig>, state: BeaconState<MinimalConfig>, eth: Eth1Data) -> signedblock
-{
-    let privkey = SecretKey::random();
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
+pub struct BeaconStateResponse {
+    pub root: String,
+    pub beacon_state: BeaconState<MinimalConfig>,
+}
 
-    let domain = beacon_node.get_domain(
-        state,
-        MinimalConfig::domai_beacon_proposer(),
-        Some(&beacon_node.get_current_epoch(state)),
-    );
-    //let randao_signature: Signature;
-    let randao_signature = Signature::new(parent.tree_hash_root(), domain, privkey);
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
+pub struct DutyInfo {
+    pub validator_pubkey: String,
+    pub attestation_slot: Slot,
+    pub attestation_committee_index: CommitteeIndex,
+    pub attestation_committee_position: ValidatorIndex,
+    pub block_proposal_slot: Option<Slot>
+}
 
-    let block = beacon_node.get_block(self.beacon_node.get_state(), randao_signature);
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
+pub struct DutiesRequest {
+    pub pubkeys: Vec<PublicKeyBytes>,
+    pub epoch: Epoch
+}
 
-    let block_confirmed = true & false | false;
+pub trait BeaconNode {
+    fn get_state(&self) -> &BeaconState<MinimalConfig>;
 
-    if block.state_hash == self.beacon_node.get_state() {
-        if block.slot > parent.slot {
-            if block.parent_root == parent.tree_hash_root() {
-                if block.body.randao_reveal = epoch_signature {
-                    block_confirmed = true;
-                }
-            }
+    fn get_current_epoch(&self, state: &BeaconState<MinimalConfig>) -> Epoch;
+
+    fn compute_start_slot_at_epoch(&self, epoch: Epoch) -> Slot;
+    
+    fn get_block_root(
+        &self,
+        state: &BeaconState<MinimalConfig>,
+        epoch: Epoch,
+    ) -> Result<H256, Error>;
+
+    fn get_duties(
+        &self,
+        validators: Vec<PublicKeyBytes>,
+        epoch: Epoch,
+    ) -> Vec<DutyInfo>;
+
+    fn publish_attestation(
+        &self,
+        attestation: Attestation<MinimalConfig>
+    ) -> Result<(), Error>;
+
+    fn get_block_root_at_slot(
+        &self,
+        state: &BeaconState<MinimalConfig>,
+        slot: Slot,
+    ) -> Result<H256, Error>;
+
+    fn get_domain(
+        &self,
+        state: &BeaconState<MinimalConfig>,
+        domain_type: DomainType,
+        message_epoch: Option<Epoch>,
+    ) -> Domain;
+
+    fn get_block(&self, slot: Slot, randao_reveal: String)->Option<BeaconBlock<MinimalConfig>>;
+}
+
+#[derive(Clone)]
+pub struct BasicBeaconNode {
+    pub beacon_node_rest_client: Rc<RestClient>,
+    last_known_state: BeaconState<MinimalConfig>,
+}
+
+impl BasicBeaconNode {
+    pub fn new() -> BasicBeaconNode {
+        let beacon_node_rest_client = Rc::new(RestClient::new(String::from("http://localhost:5052")).unwrap());
+        let state: BeaconStateResponse = beacon_node_rest_client.get(&"/beacon/state").unwrap();
+        BasicBeaconNode {
+            beacon_node_rest_client,
+            last_known_state: state.beacon_state,
+        }
+    }
+
+    pub fn update_state(&mut self) -> () {
+        let client = &self.beacon_node_rest_client;
+        match client.get(&"/beacon/state") {
+            Some(state) => self.last_known_state = state,
+            None => println!("failed update state"),
+        };
+    }
+}
+
+impl BeaconNode for BasicBeaconNode {
+    fn get_state(&self) -> &BeaconState<MinimalConfig> {
+        &self.last_known_state
+    }
+
+    fn get_block(&self, slot: Slot, randao_reveal: String) -> Option<BeaconBlock<MinimalConfig>> {
+        let url = format!("/validator/block?slot={}&randao_reveal={}", slot, randao_reveal);
+        (&self).beacon_node_rest_client.get(&url[..])
+    }
+
+    fn get_current_epoch(&self, state: &BeaconState<MinimalConfig>) -> Epoch {
+        state.slot / 8
+    }
+
+    fn compute_start_slot_at_epoch(&self, epoch: Epoch) -> Slot {
+        epoch * 8
+    }
+
+    fn get_duties(&self, validators: Vec<PublicKeyBytes>, epoch: Epoch) -> Vec<DutyInfo> {
+        let request_body = Option::Some(DutiesRequest {
+            pubkeys: validators,
+            epoch
+        });
+        (&self).beacon_node_rest_client.post("/validator/duties", request_body).unwrap()
+    }
+
+    fn publish_attestation(&self, request_body: Attestation<MinimalConfig>) -> Result<(), Error> {
+        match (&self).beacon_node_rest_client.post("/validator/attestation", Option::Some(request_body)) {
+            Some(()) => Ok(()),
+            _ => Err(Error::AttestionPublishingError)
         }
     }
 
 
-    
-    let mut signedblock = SignedBeaconBlock::new();
-    if block_confirmed{
-        signedblock.message = block;
+    fn get_block_root(
+        &self,
+        state: &BeaconState<MinimalConfig>,
+        epoch: Epoch,
+    ) -> Result<H256, Error> {
+        let slot: Slot = epoch * SLOTS_PER_EPOCH;
+        self.get_block_root_at_slot(state, slot)
     }
-    signedblock.signature = privkey;
-    
+
+    fn get_block_root_at_slot(
+        &self,
+        state: &BeaconState<MinimalConfig>,
+        slot: Slot,
+    ) -> Result<H256, Error> {
+        if slot < state.slot && state.slot <= slot + SLOTS_PER_HISTORICAL_ROOT {
+            let root_idx = (slot % SLOTS_PER_HISTORICAL_ROOT) as usize;
+            return Ok(state.historical_roots[root_idx]);
+        }
+        Err(Error::SlotOutOfRange)
+    }
+
+    fn get_domain(
+        &self,
+        state: &BeaconState<MinimalConfig>,
+        domain_type: DomainType,
+        message_epoch: Option<Epoch>,
+    ) -> Domain {
+
+        let epoch = match message_epoch {
+            Some(epoch) => epoch,
+            None => state.fork.epoch
+        };
+
+        let mut result = (domain_type as u64) << 8;
+
+        let version = if epoch < state.fork.epoch {
+            state.fork.previous_version.clone()
+        } else {
+            state.fork.current_version.clone()
+        };
+        for byte in version {
+            result = (result | (byte as u64) ) << 2; 
+        }
+        result
+    }
 }
 
 
